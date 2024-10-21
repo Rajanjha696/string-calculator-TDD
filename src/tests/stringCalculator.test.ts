cdf34927479e05636a83ac2ca2bb6424753f4511
Rajan <rajanjha6965@gmail.com>
2024-10-21 13:41:05 +0530

    Added test case to check for single input - Failing case

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -3,3 +3,7 @@ import { add } from '../stringCalculator';
 test('should return 0 for an empty string', () => {
   expect(add('')).toBe(0);
 });
+
+test('should return the number itself when one number is passed', () => {
+    expect(add('1')).toBe(1);
+});
