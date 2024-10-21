4006da4819b46647ec1f32209a20f180899733a6
Rajan <rajanjha6965@gmail.com>
2024-10-21 14:12:12 +0530

    New Test case for checking support for different delimiters - [Added logic in method] - Passing

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,13 @@
 export function add(numbers: string): number {
     if (numbers === '') return 0;
-    const numArray = numbers.replace(/\n/g, ',').split(',').map(Number);
+    
+    let delimiter = ',';
+    if (numbers.startsWith('//')) {
+      const delimiterEnd = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEnd);
+      numbers = numbers.substring(delimiterEnd + 1);
+    }
+  
+    const numArray = numbers.replace(/\n/g, delimiter).split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
