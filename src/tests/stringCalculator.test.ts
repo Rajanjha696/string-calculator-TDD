1215f1140feb65a3ad4ac51b809aabc411175c11
Rajan <rajanjha6965@gmail.com>
2024-10-21 13:47:01 +0530

    Added test cases for two comma separated numbers - Failing

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -1,9 +1,14 @@
 import { add } from '../stringCalculator';
 
 test('should return 0 for an empty string', () => {
-  expect(add('')).toBe(0);
+    expect(add('')).toBe(0);
 });
 
 test('should return the number itself when one number is passed', () => {
     expect(add('1')).toBe(1);
 });
+
+test('should return the sum of two comma-separated numbers', () => {
+    expect(add('1,5')).toBe(6);
+});
+
