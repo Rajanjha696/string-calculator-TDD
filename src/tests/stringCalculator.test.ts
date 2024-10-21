07e2ccd44d034962ea0a577b2ddfce6e16a0d0e9
Rajan <rajanjha6965@gmail.com>
2024-10-21 13:53:46 +0530

    Added test case to test multiple comma separted numbers- already handled in method - Passing

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -12,3 +12,7 @@ test('should return the sum of two comma-separated numbers', () => {
     expect(add('1,5')).toBe(6);
 });
 
+test('should return the sum of multiple numbers', () => {
+    expect(add('1,2,3,4')).toBe(10);
+});
+
